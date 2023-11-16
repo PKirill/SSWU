b4a77a203fbd5a09a919d9238391c6730cc3c8cd
kyrylopolishchuk <c-Kyrylo.Polishchuk@skykick.com>
2023-11-16 15:32:15 +0200

    add filtering

---
+++ ETSample/ETSample/CustomQueryable.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Linq.Expressions;
+
+public class CustomQueryable : IQueryable<int>
+{
+    public Type ElementType => throw new NotImplementedException();
+
+    public Expression Expression => throw new NotImplementedException();
+
+    public IQueryProvider Provider => throw new NotImplementedException();
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class CustomQueryableProvider : IQueryProvider
+{
+    public IQueryable CreateQuery(Expression expression)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object? Execute(Expression expression)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TResult Execute<TResult>(Expression expression)
+    {
+        throw new NotImplementedException();
+    }
+}
--- ETSample/ETSample/Program.cs
+++ ETSample/ETSample/Program.cs
@@ -1,12 +1,58 @@
-﻿using ETSample;
+﻿using System.Linq.Expressions;
+using System.Reflection.Metadata;
+using ETSample;
 
 var programmers = Filler.FillInProgrammers();
 
-var linqExperts = programmers.Where(p => p.Country == "France")
-    .Select(p => new LINQExpert(p));
-
-
-foreach (var expert in linqExperts)
+bool isFiltering = true;
+string filterQuery = "";
+do
 {
-    Console.WriteLine($"{expert.Programmer.Name} {expert.Programmer.Country}");
-}
+    Console.WriteLine("Enter filter query or Exit to quit");
+    filterQuery = Console.ReadLine();
+
+    if (filterQuery.Contains("exit", StringComparison.InvariantCultureIgnoreCase))
+    {
+        isFiltering = false;
+    }
+    else
+    {
+
+        var programmerParameterExpression = Expression.Parameter(typeof(Programmer));
+        Expression? filterExpression = null;
+
+        foreach (var filter in filterQuery.Split(' '))
+        {
+            var filterQueryParameters = filter.Split(':');
+            var propExpression = Expression.Property(programmerParameterExpression, filterQueryParameters[0]);
+
+            var filterValue = Convert.ChangeType(filterQueryParameters[1], typeof(Programmer).GetProperty(filterQueryParameters[0]).PropertyType);
+
+            var filterValueExpression = Expression.Constant(filterValue);
+            var filterPropExpression = Expression.Equal(propExpression, filterValueExpression);
+
+            if (filterExpression is not null)
+            {
+                var prevExpression = filterPropExpression;
+                filterExpression = Expression.And(prevExpression, filterPropExpression);
+            }
+            else
+            {
+                filterExpression = filterPropExpression;
+            }
+        }
+
+        if (filterExpression is not null)
+        {
+            var expression = Expression.Lambda<Func<Programmer, bool>>(filterExpression, false, new List<ParameterExpression>() { programmerParameterExpression });
+
+            var func = expression.Compile();
+            programmers = programmers.Where(func);
+        }
+
+        foreach (var expert in programmers)
+        {
+            Console.WriteLine($"{expert.ID} - {expert.Name} {expert.Country} {expert.Age} {expert.Date}");
+        }
+    }
+} while (isFiltering);
